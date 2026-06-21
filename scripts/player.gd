a94ac8035cda6282101aa8a72a7ef52d11392cf4
audsgegelods <161336269+audsgegelods@users.noreply.github.com>
2026-06-21 22:10:11 +0800

    Added turning velocity

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,22 +4,28 @@ extends CharacterBody2D
 
 #Movement
 @export var move_speed:= 8.0
+@export var rotation_speed:= 0.08
 var move_dir: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	$Debug.visible = show_debug
 
 func _physics_process(delta) -> void:
-	self.velocity = get_move_dir() * move_speed
+	move_dir = rotate_towards_cursor()
+	self.velocity = move_dir * move_speed
 	move_and_slide()
 
 func _process(delta) -> void:
 	update_debug_arrows()
 
-func get_move_dir() -> Vector2:
+func get_cursor_dir() -> Vector2:
 	var mouse_pos = get_viewport().get_mouse_position()
 	var dir = mouse_pos - self.position
 	return dir.normalized()
 
+func rotate_towards_cursor() -> Vector2:
+	return move_dir.lerp(get_cursor_dir(), rotation_speed).normalized()
+
 func update_debug_arrows() -> void:
-	$Debug/MoveDirArrow.rotation = get_move_dir().angle()
+	$Debug/CursorPosArrow.rotation = get_cursor_dir().angle()
+	$Debug/MoveDirArrow.rotation = rotate_towards_cursor().angle()
