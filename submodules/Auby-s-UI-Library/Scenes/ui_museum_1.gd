c3f96b875fb99800a38da9b679f94a8b764fc102
audsgegelods <161336269+audsgegelods@users.noreply.github.com>
2026-06-21 11:11:08 +0800

    untracked ui library

--- submodules/Auby-s-UI-Library/Scenes/ui_museum_1.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-signal to_page_1()
-
-func _on_switch_menu_pressed():
-	to_page_1.emit()
--- submodules/Auby-s-UI-Library/Scripts/ui_museum-0.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-signal to_page_2()
-
-func _on_switch_menu_pressed():
-	to_page_2.emit()
